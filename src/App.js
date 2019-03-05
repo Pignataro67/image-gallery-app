4f9f06e2590843deaf2cb68062a0d2ee45e7c693
Pignataro67 <Pignataro67@gmail.com>
2019-03-05 13:47:45 -0800

    adding css

--- src/App.js
+++ src/App.js
@@ -5,6 +5,15 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <div className ="navbar">
+          <p>Nav bar</p>
+        </div>
+        <div className="sidebar">
+          <p>side bar</p>
+        </div>
+        <div className="main-content">
+          <p>main content</p>
+        </div>
       </div>
     );
   }
